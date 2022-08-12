47bbec7ada345b33731bcc5f07fcd48c825ad489
desyzl123 <lu17354778157@163.com>
2022-08-12 11:47:48 +0800

    1

---
+++ pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="container">
+		<view class="title">青瑶图床</view>
+		<view class="">
+			<view class="">请上传图片</view>
+			<view>
+				<uni-file-picker v-model="imageValue" fileMediatype="image" mode="grid" @success="success" @fail="fail" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+const upload = uniCloud.importObject('upload');
+export default {
+	data() {
+		return {
+			imageValue: '',
+			imgArr: []
+		};
+	},
+	onLoad() {},
+	methods: {
+		success(e) {
+			console.log(e);
+			this.imgArr = e.tempFilePaths;
+		},
+		fail(e) {
+			console.log(e);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+	text-align: center;
+	font-size: 60rpx;
+}
+.uni-file-picker {
+	width: 400px;
+	height: 400px;
+	/deep/ .uni-file-picker__container {
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
